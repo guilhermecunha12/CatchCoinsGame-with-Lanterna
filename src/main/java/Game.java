f5f18ccb6b0e32f68ccdfb0b15d17a449ba5d4b2
Guilherme Cunha <jguilhermecunha@gmail.com>
2025-09-30 21:54:42 +0100

    Added support to read key strokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -23,21 +27,47 @@ public class Game {
             screen.doResizeIfNecessary();     // resize screen if necessary
 
             } catch (IOException e) {
-            e.printStackTrace();
+                e.printStackTrace();
             }
     }
 
     private void draw() throws IOException { // o method draw, caso falhe, lança uma exceção para a function call chain
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(this.x, this.y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() {
         try {
-            draw();
+            while (true) {
+                draw();
+                KeyStroke key = screen.readInput();
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                    screen.close();
+                } else if (key.getKeyType() == KeyType.EOF) {
+                    break;
+                }
+                processKey(key);
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case KeyType.ArrowLeft:
+                this.x--;
+                break;
+            case KeyType.ArrowRight:
+                this.x++;
+                break;
+            case KeyType.ArrowUp:
+                this.y--;
+                break;
+            case KeyType.ArrowDown:
+                this.y++;
+                break;
         }
     }
 }
